892c06850afe028a71e7b16f12c06a0388a81c71
brianhillman <brianjhillman@gmail.com>
2014-08-14 17:16:12 -0400

    added recursive support for folders, changed from bitbucket

--- pythonMoodleScraper.py
+++ pythonMoodleScraper.py
@@ -3,10 +3,22 @@ import bs4
 import os
 
 folderPath = "test"
-cookies = dict(MOODLEID1_DENTISTRY='%25C5c%2510%25BA%2581%252A',
-               MoodleSessionDENTISTRY='rnc3bh90oo4pgmtqambmsksva5',
-               MoodleSessionTestDENTISTRY='MRc1RM9jRm')
+cookies = {}
 
+def login():
+
+    login = {"username":"sntest", "password":"123456", "Content-Type":"application/x-www-form-urlencoded"}
+    r = requests.post("https://moodle.rutgers.edu/login/index.php",params=login)
+
+    cookie1 =  r.headers['set-cookie'].split(' ')[2].replace(';','')
+    cookie1 = cookie1.split('=')
+
+    cookie2 =  r.headers['set-cookie'].split(' ')[0].replace(';','')
+    cookie2 = cookie2.split('=')
+
+    cookies["MOODLEID1_DENTISTRY"] = "%25C5c%2510%25BA%2581%252A"
+    cookies[cookie1[0]] = cookie1[1]
+    cookies[cookie2[0]] = cookie2[1]
 
 def getCourseMoogle(courseName):
     courseCode = courseName.split('-', 1)[0]
@@ -38,7 +50,7 @@ def parseCourseMoogle(courseURL):
     pageBody = bodyHeader[0].parent
     weeks = pageBody.find_all("tr")
     for week in weeks:
-        print parseWeek(week)
+        print parseWeek(week).encode('utf-8')
 
 
 def parseWeek(tagTR):
@@ -108,6 +120,8 @@ def handleLI(tagLI):
     #test downloading items marked with icon.gif
     elif fileType == "icon.gif":
         fileName = downloadFile(getFileURL(tagLI))
+    elif fileType == 'folder.gif':
+        return downloadFolder(getFileURL(tagLI))
     else:
         print "Skipping unhandled filetype: ", fileType
         return ""
@@ -130,9 +144,12 @@ def getFileURL(tagLI):
 
 def downloadFile(moodleURL):
     #print "downloading...", moodleURL
+    #http://moodle.rutgers.edu/mod/resource/view.php?id=175920
+    #
+    #
+
 
     fileURL  = requests.get(moodleURL, cookies = cookies, allow_redirects = False)
-    folder   = fileURL.url[fileURL.url.rfind('=')+1:]
     try:
         fileURL  = str(fileURL.headers['location'])
     except:
@@ -155,10 +172,59 @@ def downloadFile(moodleURL):
             writeFile.write(chunk)
     return fileName
 
+def downloadFolder(url):
+    print url
+    response = requests.get(url, cookies=cookies)
+    soup = bs4.BeautifulSoup(response.text)
 
+    listOfFiles = soup.find("table", class_="files")
+    try:
+        listOfFiles = listOfFiles.find_all("a")
+    except:
+        return ""
+    result = '<blockquote style="margin: 0px 0px 0px 40px; border: none; padding: 0px;">'
+    for aTAg in listOfFiles:
+            imgTag = aTAg.find('img')
+            fileType = str(imgTag['src'])[str(imgTag["src"]).rfind('/')+1:]
+            fileURL = aTAg['href']
+
+            if fileType == "folder.gif":
+                result += downloadFolder("http://moodle.rutgers.edu/mod/resource/"+str(fileURL))
+                continue
+
+            fileURL = aTAg['href']
+            fileName = fileURL[fileURL.rfind('/')+1:]
+            fileURL  = requests.get(fileURL, cookies = cookies)
+            #hacky way to replace % escaped characters, well at least spaces haha
+            fileName = fileName.replace("%20", " ")
+
+
+            #print "downloading ",fileURL.url, "As: ", fileName
+            try:
+                open(folderPath+'/'+fileName, 'wb')
+            except:
+                os.mkdir(folderPath)
+
+            with open(folderPath+'/'+fileName, 'wb') as writeFile:
+                for chunk in fileURL.iter_content(512):
+                    writeFile.write(chunk)
+            result += '&nbsp;<a href="/CurrentCourse/' + fileName + '" target="_new">' + aTAg.get_text() + '&nbsp;</a><br />\n'
+
+
+    return result + '</blockquote>'
 
 
 if __name__ == '__main__':
-    siteURL    = 'http://moodle.rutgers.edu/course/view.php?id=2692'
+    #login()
+    #print cookies
+    cookies['MOODLEID1_DENTISTRY'] = "%25C5c%2510%25BA%2581%252A"
+    cookies['MoodleSessionDENTISTRY'] = "91ea3r8l29g72psr6n2t0f5es0"
+    cookies['MoodleSessionTestDENTISTRY'] = 'XEbJybtYNu'
+    siteURL    = 'http://moodle.rutgers.edu/course/view.php?id='
+    mCode = str(input("Enter the 4 digit course moodle code: "))
+    siteURL += mCode
     folderPath = siteURL[siteURL.rfind('=')+1:]
     parseCourseMoogle(siteURL)
+
+
+    # testing folders
